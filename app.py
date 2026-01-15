0cfffc4878fc3faab49e4fcd6746c4e796ee314e
Diego <diegoorellanadoctors@gmail.com>
2026-01-15 02:35:47 -0300

    ''

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 with st.echo():
     st.title("CAT")
 
-    st.markdown("[![Click me](app/static/mineria.jpg)](https://streamlit.io)")
+    st.markdown("[![Click me](app/static/mineria.png)](https://streamlit.io)")
